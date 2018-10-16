70f428c86c4476867fa60e740967b958bc14c723
kaiplanet <hang3126@sina.com>
2018-10-16 12:19:12 +0800

    fix netease proxy

--- site/controllers/proxy.js
+++ site/controllers/proxy.js
@@ -2,7 +2,8 @@
  * Created by qhyang on 2018/4/27.
  */
 
-const https = require('https'),
+const http = require('http'),
+    https = require('https'),
     url = require('url');
 
 const proxy = require('http-proxy-middleware'),
@@ -46,13 +47,69 @@ module.exports = {
             })(req, res)
         });
 
-        app.use('/netease', cache('24 hours'), proxy({
-            target: 'http://m10.music.126.net',
-            pathRewrite: {
-                '^/netease' : ''
-            },
-            changeOrigin: true
-        }));
+        app.use('/netease', cache('24 hours'), async (req, res, next) => {
+            let redirectLocation = url.parse(`http://music.163.com${req.url}`);
+
+            const sendReq = location => {
+                const client = (protocol => {
+                    switch (protocol) {
+                        case 'http:':
+                            return http;
+                        case 'https:':
+                        default:
+                            return https;
+                    }
+                })(location.protocol);
+
+                return new Promise(resolve => {
+                    client.get(location.href, res => resolve(res));
+                });
+            };
+
+            while (true) {
+                const originRes = await sendReq(redirectLocation);
+
+                if (originRes.statusCode >= 200 && originRes.statusCode < 300) {
+                    const location = redirectLocation;
+
+                    req.originalUrl = location.href;
+
+                    if (!location.protocol) {
+                        location.protocol = 'https:';
+                    }
+
+                    if (!location.host) {
+                        location.host = 'm10.music.126.net';
+                    }
+
+                    req._targetLocation = location;
+
+                    next();
+
+                    break;
+                } else if (originRes.statusCode >= 300 && originRes.statusCode < 400 && originRes.headers.location) {
+                    const location = url.parse(originRes.headers.location);
+
+                    if (!location.protocol) {
+                        location.protocol = 'https:';
+                    }
+
+                    if (!location.host) {
+                        location.host = 'm10.music.126.net';
+                    }
+
+                    redirectLocation = location;
+                } else {
+                    throw 'Fetch error';
+                }
+            }
+
+        }, (req, res) => {
+            proxy({
+                target: `${req._targetLocation.protocol}//${req._targetLocation.host}`,
+                changeOrigin: true
+            })(req, res);
+        });
 
         app.use('/qq', cache('24 hours'), proxy({
             target: 'http://dl.stream.qqmusic.qq.com',
@@ -62,9 +119,9 @@ module.exports = {
         app.use('/hearthis', cache('24 hours'), (req, res, next) => {
             https.get(`https://hearthis.at${req.url}`, originRes => {
                 if (originRes.statusCode > 300 && originRes.statusCode < 400 && originRes.headers.location) {
-                    location = url.parse(originRes.headers.location);
+                    const location = url.parse(originRes.headers.location);
 
-                    req.originalUrl = location.path;
+                    req.originalUrl = location.href;
 
                     if (!location.protocol) {
                         location.protocol = 'https:';
@@ -88,7 +145,7 @@ module.exports = {
             proxy({
                 target: `${req._targetLocation.protocol}//${req._targetLocation.host}`,
                 changeOrigin: true
-            })(req, res)
+            })(req, res);
         });
 
         app.use('/proxy', cache('24 hours'), (req, res) => {
@@ -100,7 +157,7 @@ module.exports = {
                     return url.slice(url.indexOf('/', 1));
                 },
                 changeOrigin: true
-            })(req, res)
+            })(req, res);
         });
     }
 };
