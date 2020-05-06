5ccf1f30d3a384a80c445996f730f57334680008
kaiplanet <hang3126@sina.com>
2020-05-06 15:25:41 +0800

    fix browser service bugs

--- src/services/BrowserService/index.ts
+++ src/services/BrowserService/index.ts
@@ -5,40 +5,73 @@ import { Browser, launch, PageCloseOptions } from "puppeteer";
 export default class {
     private browser?: Browser;
     private proxy?: string;
+    private locked = false;
 
     public async createBrowserPage({ proxy }: { proxy?: string } = {}) {
-        if (this.browser) {
-            if (this.proxy !== proxy) {
-                if (this.browser && (await this.browser.pages()).length <= 1) {
-                    this.browser.close();
-                }
-
-                this.browser = await this.getBrowserInstance({ proxy });
-            }
-
-            const page = await this.browser.newPage();
-            const that = this;
-            const browser = that.browser;
-            const close = page.close;
-
-            page.close = async function(options?: PageCloseOptions) {
-                await close.apply(this, [options]);
-
-                if (browser && (await browser.pages()).length <= 1) {
-                    browser.close();
-
-                    if (that.browser === browser) {
-                        delete that.browser;
-                    }
-                }
-            };
-
-            return page;
+        while (this.locked) {
+            await new Promise(((resolve) => setTimeout(resolve, 0)));
         }
 
-        this.browser = await this.getBrowserInstance({ proxy });
+        this.locked = true;
 
-        return this.browser.newPage();
+        try {
+            if (this.browser) {
+                if (this.proxy !== proxy) {
+                    if (this.browser && (await this.browser.pages()).length <= 1) {
+                        this.browser.close();
+                    }
+
+                    this.browser = await this.getBrowserInstance({ proxy });
+                }
+
+                const page = await this.browser.newPage();
+                const that = this;
+                const browser = that.browser;
+                const close = page.close;
+
+                page.close = async function(options?: PageCloseOptions) {
+                    while (that.locked) {
+                        await new Promise(((resolve) => setTimeout(resolve, 0)));
+                    }
+
+                    that.locked = true;
+
+                    try {
+                        await close.apply(this, [options]);
+
+                        if (browser && (await browser.pages()).length <= 1) {
+                            browser.close();
+
+                            if (that.browser === browser) {
+                                delete that.browser;
+                            }
+                        }
+
+                        that.locked = false;
+                    } catch (e) {
+                        that.locked = false;
+
+                        throw e;
+                    }
+                };
+
+                this.locked = false;
+
+                return page;
+            }
+
+            this.browser = await this.getBrowserInstance({ proxy });
+
+            const newPage = await this.browser.newPage();
+
+            this.locked = false;
+
+            return newPage;
+        } catch (e) {
+            this.locked = false;
+
+            throw e;
+        }
     }
 
     private async getBrowserInstance({ proxy }: { proxy?: string } = {}) {
